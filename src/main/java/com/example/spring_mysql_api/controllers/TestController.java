7b8aa799ad5d65f3db72a4ae60ccfdd444f97658
Chris_nkuutu <Knightfall7@users.noreply.github.com>
2024-01-10 01:37:58 +0100

    Addeed TestController

--- src/main/java/com/example/spring_mysql_api/controllers/TestController.java
+++ src/main/java/com/example/spring_mysql_api/controllers/TestController.java
@@ -1,2 +1,13 @@
-package com.example.spring_mysql_api.controllers;public class TestController {
+package com.example.spring_mysql_api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String getTest(){
+        return  "This is to test that we added a separate branch";
+    }
 }
