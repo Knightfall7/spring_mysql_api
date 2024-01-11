9be4a6d52636de2b13a7c3ee4c37aacad0a58682
Chris_nkuutu <Knightfall7@users.noreply.github.com>
2024-01-11 14:18:10 +0100

    created conflicts controller

--- src/main/java/com/example/spring_mysql_api/controllers/ConflictsController.java
+++ src/main/java/com/example/spring_mysql_api/controllers/ConflictsController.java
@@ -1,21 +1,24 @@
 package com.example.spring_mysql_api.controllers;
 
+import org.apache.catalina.users.SparseUserDatabase;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ConflictsController {
     @GetMapping("/conflicts")
     public String getConflicts() {
         return "Getting conflicts";
-    }
-    @PostMapping("/conflicts") // Create a conflicts
-    public String postConflicts() {
-        return "Posting conflicts: ";
-    }
 
     }
     @DeleteMapping("/conflicts")
     public String deleteConflicts(){
-        return "Deleting conflicts";
+        return "Deleting Conflicts";
+    }
+    @PostMapping("/conflicts")
+    public String postConflicts(){
+        return "Posting Conflicts";
     }
 
+}
