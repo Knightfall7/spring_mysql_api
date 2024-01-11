10432f61d8609c5c69d3a513cc2ea8dc9e89edb0
Chris_nkuutu <Knightfall7@users.noreply.github.com>
2024-01-11 13:47:56 +0100

    created post conflicts method

--- src/main/java/com/example/spring_mysql_api/controllers/ConflictsController.java
+++ src/main/java/com/example/spring_mysql_api/controllers/ConflictsController.java
@@ -1,12 +1,18 @@
 package com.example.spring_mysql_api.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ConflictsController {
     @GetMapping("/conflicts")
     public String getConflicts() {
         return "Getting conflicts";
-
     }
+    @PostMapping("/conflicts") // Create a conflicts
+    public String postConflicts() {
+        return "Posting conflicts: ";
+    }
+
 }
